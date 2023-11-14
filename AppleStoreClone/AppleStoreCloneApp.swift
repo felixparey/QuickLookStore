d3cdbedbba10d0c6afde35e6a15621fb62639044
felixparey <mysteryparey@gmail.com>
2023-11-14 15:49:28 +0100

    Initial Commit

---
+++ AppleStoreClone/AppleStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStoreCloneApp.swift
+//  AppleStoreClone
+//
+//  Created by Felix Parey on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleStoreClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleStoreClone
+//
+//  Created by Felix Parey on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
