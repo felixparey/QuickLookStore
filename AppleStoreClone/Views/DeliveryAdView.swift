dd47ed5488a3bab0f85f4a3dead6d408f1851bf8
felixparey <mysteryparey@gmail.com>
2023-11-20 10:47:06 +0100

    Optimised for iPad and iPhone now kind of

---
+++ AppleStoreClone/Views/DeliveryAdView.swift
@@ -0,0 +1,18 @@
+//
+//  DeliveryAdView.swift
+//  AppleStoreClone
+//
+//  Created by Felix Parey on 16/11/23.
+//
+
+import SwiftUI
+
+struct DeliveryAdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeliveryAdView()
+}
---
+++ AppleStoreClone/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  AppleStoreClone
+//
+//  Created by Felix Parey on 17/11/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
